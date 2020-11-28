bbc91cec90fc499eed0c417988f365ce529faac7
zed <zed@mail.ru>
2020-11-29 02:11:11 +0300

    added some tests

--- tests.py
+++ tests.py
@@ -1,14 +1,32 @@
-"""
-app tests example
-
-"""
-
-from app import is_alive_host
+import unittest
+from requests import Response
+from host_checker import is_alive_host
 
 
-def test_live():
-    assert is_alive_host('semrush.com')
+class TestHostCheckerFunc(unittest.TestCase):
+    def test_200_codes(self):
+        res = Response()
+        res.status_code = 200
+        self.assertTrue(is_alive_host(response=res))
+
+    def test_300_codes(self):
+        res = Response()
+        res.status_code = 300
+        self.assertTrue(is_alive_host(response=res))
+
+    def test_400_codes(self):
+        res = Response()
+        res.status_code = 400
+        self.assertFalse(is_alive_host(response=res))
+
+    def test_500_codes(self):
+        res = Response()
+        res.status_code = 500
+        self.assertFalse(is_alive_host(response=res))
+
+    def test_no_args(self):
+        self.assertRaises(AttributeError, is_alive_host)
 
 
-def test_down():
-    assert not is_alive_host('invalid.domain.son')
+if __name__ == "__main__":
+    unittest.main()
