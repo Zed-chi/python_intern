942519bc61a89b1136c31d8f3d735d2ff4957100
zed <zed@mail.ru>
2020-11-29 02:10:06 +0300

    changed func to non async to prevent blocking

--- api.py
+++ api.py
@@ -1,17 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from host_checker import check_and_correct_host, is_alive_host
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello, provide hostname=... to /healthz path"}
+
 
 @app.get("/healthz")
-async def root(hostname):
-    if not hostname:
-        return {"message": "Invalid Hostname"}
-    hostname = check_and_correct_host(hostname)
-    alive = is_alive_host(hostname)    
-    return {"status": "up" if alive else "down"}
+def root(hostname=Query(None)):
+    try:
+        if not hostname:
+            raise()
+        hostname = check_and_correct_host(hostname)
+        is_alive = is_alive_host(hostname)
+        return {"status": "up" if is_alive else "down"}
+    except Exception:
+        return {"message": "Error, invalid hostname value"}
+    
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="127.0.0.1", port=5000, log_level="info")
